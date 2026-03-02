a89dff5155bf5373063ffd87be4e31a6e7ff592c
Souvik313 <roysouvik8220@gmail.com>
2026-03-02 17:57:51 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://image-gen-delta-one.vercel.app/",
+    credentials: true,
+}
+));
 app.use(express.json({limit: "50mb"}));
 
 app.use("/api/v1/post" , postRouter);
